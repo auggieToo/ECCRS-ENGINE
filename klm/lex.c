ed3d282197e224c7b3b9d1e6b7486927610fbdfa
auggietoo <august2kam@gmail.com>
2026-07-26 22:14:32 +0200

    base rank impl

--- klm/lex.c
+++ klm/lex.c
@@ -9,10 +9,6 @@ PicoSAT *solver;
 typedef struct 
 {
 	knowledgeBase *r; 
-	u32 size; 
-
-	//resizing 
-	u32 _capacity; 
 
 }ruleRank; 
 
@@ -27,7 +23,8 @@ typedef struct
 }orderedTuple;
 
 i8 tupleInit(orderedTuple *ot);
-
+i8 tupleNewRank(orderedTuple *ot); 
+i8 tupleAddFormula(orderedTuple *ot, rule r);
 
 //return true if 'concl' is a logical consequence of 'premise'
 static u8 logicalConsequence(formula premise, formula concl);
@@ -35,7 +32,7 @@ static u8 logicalConsequence(formula premise, formula concl);
 
 //return true if the implication r is entailed
 //by the knowledge base K, 
-static u8 Entail(knowledgeBase K , implic r);
+static u8 entail(knowledgeBase K , formula r);
 
 //materialize the knowledge base 
 //turn every defeasible implication into a classical 
@@ -48,7 +45,8 @@ BaseRank(knowledgeBase K)
 {
 	//initialize our solver 
 	solver = picosat_init();
-	orderedTuple ot; 	
+	orderedTuple ot; 
+	tupleInit(&ot);
 
 	//algorithm for  BaseRank
 	knowledgeBase kArrow = {0};
@@ -57,13 +55,41 @@ BaseRank(knowledgeBase K)
 	materialisation(K, &kArrow);
 
 	u32 i = 0; 
+	
+	//E_o = K^(->) 
 	knowledgeBase E_i = kArrow; 
+
 	do {
+		//an array to keep track of the formula we inserted 
+		//in the current rank 
+		u32 ranks[E_i.count];
+		i32 size = -1;
 		
-		for(int k = 0 ; k < E_i.count ; k++
+		
+		//init new rank
+		tupleNewRank(&ot);
+
+		knowledgeBase E_i1;
+		kbInit(&E_i1, 4);
+		
+		//E_(i+1) = {a -> B in E_i | E_i entails not a}
+		for(int k = 0 ; k < E_i.count ; k++)
 		{
-			knowledgeBase E_i1;
+			if(entail(E_i, E_i.rules[k].impl.body ))
+			{
+				kbAddRule(&E_i1, 
+						  E_i.rules[k].impl.type, 
+						  &E_i.rules[k].impl.head ,
+						  &E_i.rules[k].impl.body);
+				ranks[++size] = k;
+			}
+			
+			//R_i = E_i \ E_(i+1)
+			//R_i is all the rules in the E_i that were not added in E_i1 		
+			tupleAddFormula(&ot, E_i.rules[k]);
+
 		}
+}
 		
 
 	}while ()
@@ -83,5 +109,41 @@ void materialisation(knowledgeBase K, knowledgeBase *out)
 		out->rules[i] = K.rules[i]; 
 		out->rules[i].impl.type = CLASSICAL;  
 	}
+}
+
+i8 
+tupleInit(orderedTuple *ot)
+{
+	
+	ot->_capacity = 8;
+
+	ot->R = malloc(ot->_capacity*sizeof(ruleRank));
+	ot->n = 0;
+		
+}
+
+i8
+tupleNewRank(orderedTuple *ot)
+{	
+	if(ot->n == ot->_capacity)
+	{
+		//grow 
+		ot->_capacity = ot->_capacity * 1.5f ;
+		ruleRank *nf = realloc(&ot->R, ot->_capacity * sizeof(ruleRank));
+		if(!nf) return -1;
+		
+		ot->R = nf; 
+	}
+	ot->n++;
+	return 0;
+
+}
+
+
+//add formula to the latest rank 
+i8 
+tupleAddFormula(orderedTuple *ot, rule r)
+{
+	kbAddRule(ot->R[ot->n].r, r.impl.type, &r.impl.head, &r.impl.body);
 }
 
